8fe4488cc4dcccbb181fe5eb1b4b9eeb36b76e82
ASFillah <2131710001@student.polinema.ac.id>
2023-11-09 15:50:54 +0700

    fixed commit

--- app/src/main/java/com/mobiledev/hellojetpackcompose/ui/theme/Theme.kt
+++ app/src/main/java/com/mobiledev/hellojetpackcompose/ui/theme/Theme.kt
@@ -18,23 +18,17 @@ import androidx.core.view.WindowCompat
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    primaryContainer = Navy,
+    onPrimaryContainer = Chartreuse
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    secondary = PurpleGrey80,
+    tertiary = Pink40,
+    primaryContainer = LightBlue,
+    onPrimaryContainer = Navy
 )
 
 @Composable
